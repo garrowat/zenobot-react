a42912e9215d897e667f612fd87d496244a378fa
GW <garrettbauer@gmail.com>
2018-07-05 13:16:05 +0900

    Added home, info, git links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import CodeIcon from '@material-ui/icons/Code';
+import InfoIcon from '@material-ui/icons/Info';
 import Hidden from '@material-ui/core/Hidden';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
@@ -19,10 +22,27 @@ const apiUrl = `${process.env.REACT_APP_API_URL}`;
 const styles = theme => ({
   root: {
     textAlign: 'center',
-    paddingTop: theme.spacing.unit * 20,
+    paddingTop: theme.spacing.unit * 10,
+  },
+  homeLink: {
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    margin: theme.spacing.unit * 3,
+  },
+  projectLinks: {
+    margin: 'auto',
+  },
+  botImageContainer: {
+    margin: 'auto',
+    marginTop: theme.spacing.unit * 6,
+    marginBottom: theme.spacing.unit * 6,
+    maxWidth: '311px',
+    height: 'auto',
   },
   botImage: {
-    margin: theme.spacing.unit * 6,
+    width: '100%',
+    height: 'auto',
   },
   proverbBox: {
     margin: theme.spacing.unit * 6,
@@ -101,6 +121,15 @@ class Index extends React.Component {
 
     return (
       <div className={classes.root}>
+
+        <div className={classes.homeLink}>
+          <a href="https://garrettwatson.io" style={{textDecoration: "none"}}>
+            <Button color="primary">
+              Go to garrettwatson.io
+            </Button>
+          </a>
+        </div>
+
         <Hidden smDown>
           <Typography variant="display4">
             <span style={{color:'#FEB415'}}>Z</span>enoBot
@@ -108,12 +137,23 @@ class Index extends React.Component {
         </Hidden>
         <Hidden mdUp>
           <Typography variant="display3">
-            ZenoBot
+            <span style={{color:'#FEB415'}}>Z</span>enoBot
           </Typography>
         </Hidden>
-        <div className={classes.botImage}>
-          <img src={zenobot} />
+
+        <div className={classes.projectLinks}>
+          <a href="https://github.com/garrowat/zenobot-react" style={{textDecoration: "none"}}>
+            <CodeIcon color="primary" />
+          </a>
+          <a href="" style={{textDecoration: "none"}}>
+            <InfoIcon color="primary" />
+          </a>
+
         </div>
+        <div className={classes.botImageContainer}>
+          <img src={zenobot} className={classes.botImage} />
+        </div>
+
         <div className={classes.description}>
           <Typography variant="body2" style={{ fontFamily: "vt323", fontSize: "1.5em"}}>
             What troubles you, friend?
@@ -126,6 +166,7 @@ class Index extends React.Component {
             />
           </form>
         </div>
+
         <Paper className={classes.proverbBox}>
           {
             isLoading
@@ -138,6 +179,7 @@ class Index extends React.Component {
             </Typography>
           }
         </Paper>
+
         <ExpansionPanel className={classes.expansionPanel}>
           <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
             <Typography variant="body2" color="textSecondary">
@@ -155,8 +197,7 @@ class Index extends React.Component {
                         {currentProverb}
                       </Typography>
                   </li>                
-                )
-                
+                )  
               }
             </ul>
           </ExpansionPanelDetails>
